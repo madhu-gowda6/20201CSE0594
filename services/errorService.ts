6ce69c7986dc976aeb8e1fa5290adae343266533
Madhu Gowda <142603446+madhu-gowda6@users.noreply.github.com>
2024-01-08 20:05:39 +0530

    Added errorservice amd apiservices

---
+++ services/errorService.ts
@@ -0,0 +1,32 @@
+import { useMemo } from 'react';
+
+import { useTranslation } from 'next-i18next';
+
+import { ErrorMessage } from '@/types/error';
+
+const useErrorService = () => {
+  const { t } = useTranslation('chat');
+
+  return {
+    getModelsError: useMemo(
+      () => (error: any) => {
+        return !error
+          ? null
+          : ({
+              title: t('Error fetching models.'),
+              code: error.status || 'unknown',
+              messageLines: error.statusText
+                ? [error.statusText]
+                : [
+                    t(
+                      'OpenAI may be experiencing issues.',
+                    ),
+                  ],
+            } as ErrorMessage);
+      },
+      [t],
+    ),
+  };
+};
+
+export default useErrorService;
